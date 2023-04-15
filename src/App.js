06285da64d125dbc56d19ec9f4f12b078743b0ef
Wayne Fuchs <wayne.fuchs@icloud.com>
2023-04-14 20:13:56 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Image from "./Image";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Welcome</h1>
+      </header>
+
+      <main>
+        <p>main content</p>
+        <Image />
+      </main>
+
+      <footer>
+        <p>Footer</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Image.js
@@ -0,0 +1,11 @@
+function Image() {
+  return (
+    <>
+      <a href="#">
+        <img src="images/whereswalle.jpeg" alt="Many Robots" ismap="true" />
+      </a>
+    </>
+  );
+}
+
+export default Image;
